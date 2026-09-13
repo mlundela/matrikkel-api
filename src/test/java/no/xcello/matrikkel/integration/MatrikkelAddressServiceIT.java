a49422812e4beff7d16b8edb92d9ebfea5f5d43b
Mads Andrè Lundeland <lundeland@gmail.com>
2026-09-13 14:04:51 +0200

    Refactoring

--- src/test/java/no/xcello/matrikkel/integration/MatrikkelAddressServiceIT.java
+++ src/test/java/no/xcello/matrikkel/integration/MatrikkelAddressServiceIT.java
@@ -35,21 +35,21 @@ class MatrikkelAddressServiceIT {
         assertThat(units)
                 .extracting(Section::nummer, Section::fraction, Section::bruksenhetNummer)
                 .containsExactly(
-                        tuple(1, brøk(42), "U0101"),
-                        tuple(2, brøk(39), "H0101"),
-                        tuple(3, brøk(53), "H0102"),
-                        tuple(4, brøk(43), "H0201"),
-                        tuple(5, brøk(56), "H0202"),
-                        tuple(6, brøk(43), "H0301"),
-                        tuple(7, brøk(56), "H0302"),
-                        tuple(8, brøk(37), "L0101"),
-                        tuple(9, brøk(57), "L0102"));
+                        tuple(1, fraction(42), "U0101"),
+                        tuple(2, fraction(39), "H0101"),
+                        tuple(3, fraction(53), "H0102"),
+                        tuple(4, fraction(43), "H0201"),
+                        tuple(5, fraction(56), "H0202"),
+                        tuple(6, fraction(43), "H0301"),
+                        tuple(7, fraction(56), "H0302"),
+                        tuple(8, fraction(37), "L0101"),
+                        tuple(9, fraction(57), "L0102"));
         assertThat(units.stream().mapToLong(s -> s.fraction().teller()).sum())
                 .as("sameiebrøker sum to the whole")
                 .isEqualTo(NEVNER);
     }
 
-    private static Fraction brøk(long teller) {
+    private static Fraction fraction(long teller) {
         return new Fraction(teller, NEVNER);
     }
 }
